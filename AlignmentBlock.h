97e907222a8a6d40127ad7407fc49ccf4237ebf4
Mark Chaisson <mchaisso@usc.edu>
2018-07-04 07:49:47 -0700

    Missing file
    	new file:   AlignmentBlock.h

---
+++ AlignmentBlock.h
@@ -0,0 +1,42 @@
+#ifndef ALIGNMENT_BLOCK_H_
+#define ALIGNMENT_BLOCK_H_
+
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+class Block {
+ public:
+	//
+	// An alignment is a collection of blocks. The qPos and tPos in a block
+	// is relative to the beginning of the alignment rather than the
+	// target or query.
+	//
+	 
+	GenomePos qPos, tPos, length;
+	Block() {qPos=tPos=length=0;}
+  Block(GenomePos q, GenomePos t, GenomePos l) : qPos(q), tPos(t), length(l) {}
+	Block& Assign(Block &rhs) {
+		qPos = rhs.qPos;
+		tPos = rhs.tPos;
+		length = rhs.length;
+		return *this;
+	}
+
+	GenomePos QEnd() {
+		return qPos + length;
+	}
+
+	GenomePos TEnd() {
+		return tPos + length;
+	}
+
+  void Clear() {
+    qPos = tPos =  length = 0;
+  }
+};
+
+
+
+#endif
