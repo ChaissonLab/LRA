e7c85b8f100acdadd0f659217fa2650ff9d608d5
Jingwen Ren <jingwenr@hpc-cmb.hpcc.usc.edu>
2020-07-11 22:12:45 -0700

    Add LinearExtension function for the 3rd SDP

--- LinearExtend.h
+++ LinearExtend.h
@@ -14,10 +14,10 @@ public:
 	vector<int> *matchesLengths;
 	int strand;
 
-  	LongAnchors(Cluster *m) {
-  		matches=&(m->matches);
-  		strand=m->strand;
-  		matchesLengths=&(m->matchesLengths);
+  	LongAnchors(GenomePairs *ms, vector<int> *mLs, int str) {
+  		matches=ms;
+  		strand=str;
+  		matchesLengths=mLs;
 	}	
 	//
 	// Cartesian sort of clusters. NOTE: if strand==1, sort on reverse
@@ -46,7 +46,8 @@ public:
 };
 
 
-void Checkbp(GenomePair & cur, GenomePair & next, Genome & genome, Read & read, int & ChromIndex, GenomePos & qe, GenomePos & te, int & strand, Options & opts, int & mat) {
+void Checkbp(GenomePair &cur, GenomePair &next, Genome &genome, Read &read, int &ChromIndex, GenomePos& qe, GenomePos& te, 
+				int &strand, Options &opts, int &mat) {
 	GenomePos curQ, curT, nextQ, nextT;
 	if (strand == 0) {
 		curQ = cur.first.pos + opts.globalK;
@@ -354,8 +355,8 @@ TrimOverlappedAnchors(vector<Cluster> & extCluster) {
 	// Get all long anchors
 	//
 	for (int c = 0; c < extCluster.size(); c++) {
-
-		LongAnchors longanchors(&extCluster[c]);
+		LongAnchors longanchors(&(extCluster[c].matches), &(extCluster[c].matchesLengths), extCluster[c].strand);
+		//LongAnchors longanchors(extCluster[c].matches&extCluster[c]);
 		for (int ln = 0; ln < extCluster[c].matches.size(); ln++) {
 			if (extCluster[c].matchesLengths[ln] >= 50) { // 500
 				longanchors.anchorIndex.push_back(ln);
@@ -426,168 +427,126 @@ TrimOverlappedAnchors(vector<Cluster> & extCluster) {
 
 
 
-// //
-// // This function trim overlapped long anchors.
-// //
-// void 
-// TrimOverlappedAnchors(vector<Cluster> & extCluster) {
-
-// 	//
-// 	// Get all long anchors
-// 	//
-// 	for (int c = 0; c < extCluster.size(); c++) {
-
-// 		LongAnchors longanchors(&extCluster[c].matches);
-// 		for (int ln = 0; ln < extCluster[c].matches.size(); ln++) {
-// 			if (extCluster[c].matchesLengths[ln] >= 50) { // 500
-// 				longanchors.anchorIndex.push_back(ln);
-// 			}
-// 		}
-
-// 		//
-// 		// Catersiansort anchors and trim overlapped adjacent two;
-// 		//
-// 		longanchors.Sort();
-// 		for (int ln = 1; ln < longanchors.anchorIndex.size(); ln++) {
-
-// 			int prev = longanchors.anchorIndex[ln-1];
-// 			int cur = longanchors.anchorIndex[ln];
-
-// 			//
-// 			// If the adjacent anchors are overlapped less than 30b, trim the prev
-// 			// 
-// 			int overlap_r = 0, overlap_g = 0, overlap = 0;
-// 			bool lr = 0, lg = 0;
-// 			if (extCluster[c].strand == 0) {
-// 				if (extCluster[c].matches[cur].first.pos < extCluster[c].matches[prev].first.pos + extCluster[c].matchesLengths[prev] 
-// 					and 
-// 					extCluster[c].matches[cur].first.pos >= extCluster[c].matches[prev].first.pos + extCluster[c].matchesLengths[prev] - 30) {
-
-// 					overlap_r = extCluster[c].matches[prev].first.pos + extCluster[c].matchesLengths[prev] - extCluster[c].matches[cur].first.pos;
-// 					assert(overlap_r <= 30); assert(overlap_r > 0);
-// 					lr = 1;
-// 				}
-// 			}
-// 			else {
-// 				if (extCluster[c].matches[cur].first.pos + extCluster[c].matchesLengths[cur] > extCluster[c].matches[prev].first.pos
-// 					and 
-// 					extCluster[c].matches[prev].first.pos <= extCluster[c].matches[cur].first.pos + extCluster[c].matchesLengths[cur] - 30) {
-
-// 					overlap_r = extCluster[c].matches[cur].first.pos + extCluster[c].matchesLengths[cur] - extCluster[c].matches[prev].first.pos;
-// 					assert(overlap_r <= 30); assert(overlap_r > 0);
-// 					lr = 1;
-// 				}				
-// 			}
-// 			if (extCluster[c].matches[cur].second.pos < extCluster[c].matches[prev].second.pos + extCluster[c].matchesLengths[prev] 
-// 				and 
-// 				extCluster[c].matches[cur].second.pos >= extCluster[c].matches[prev].second.pos + extCluster[c].matchesLengths[prev] - 30) {
-
-// 				overlap_g = extCluster[c].matches[prev].second.pos + extCluster[c].matchesLengths[prev] - extCluster[c].matches[cur].second.pos;
-// 				assert(overlap_g <= 30); assert(overlap_g > 0);
-// 				lg = 1;
-// 			}
-// 			if (overlap_r > 0 or overlap_g > 0) {
-// 				overlap = max(overlap_r, overlap_g);
-// 				if (extCluster[c].strand == 0) extCluster[c].matchesLengths[prev] -= overlap+1;
-// 				else extCluster[c].matchesLengths[cur] -= overlap+1;
-
-// 				if (lr == 1) {
-// 					if (extCluster[c].strand == 0) assert(extCluster[c].matches[cur].first.pos >= extCluster[c].matches[prev].first.pos + extCluster[c].matchesLengths[prev]);
-// 					else assert(extCluster[c].matches[prev].first.pos >= extCluster[c].matches[cur].first.pos + extCluster[c].matchesLengths[cur]);
-// 				}
-// 				if (lg == 1) assert(extCluster[c].matches[cur].second.pos >= extCluster[c].matches[prev].second.pos + extCluster[c].matchesLengths[prev]);
-// 			}		
-// 		}
-// 	}
-// }
-
-
-/*
 //
 // This function just do linear extension without avoiding overlapping points;
+// Input: GenomePairs pairs; Output: GenomePairs ExtendPairs; vector<int> ExtendPairsMatchesLength;
+// NOTE: Only forward strand;
 //
-void LinearExtend(vector<Cluster*> clusters, vector<Cluster> & extCluster, vector<unsigned int> & chain, Options & opts, Genome & genome, Read & read) {
+void LinearExtend(GenomePairs &pairs, GenomePairs & Extendpairs, vector<int> &ExtendpairsMatchesLength, Options &opts, Genome &genome, Read &read, int &chromIndex) {
+	//
+	// Sort each Cluster
+	//
+	DiagonalSort<GenomeTuple>(pairs.begin(), pairs.end());
+	//
+	// Linear Extension;
+	// NOTICE: anchors have the same strand;
+	//
+	int n = 1;
+	int m = 0;
+	int strand = 0;
+	int mat = 0;
+	while (n < pairs.size()) {
 
-	for (int c = 0; c < chain.size(); c++) {
+		long long int curDiag, nextDiag;
+		curDiag = (long long int) pairs[n-1].first.pos - (long long int) pairs[n-1].second.pos;
+		nextDiag = (long long int) pairs[n].first.pos - (long long int) pairs[n].second.pos;
 
-		int cm = chain[c];
+		if (curDiag == nextDiag) { // those two anchors are on the same diagonal
 
-		//
-		// Compute the ChromIndex for cluster
-		//
-		extCluster[c].chromIndex = clusters[cm]->chromIndex;
-
-		//
-		// Sort each Cluster
-		// NOTICE: if the current Cluster get more anchors in the btwn space or end space, we need to sort them. Otherwise we do not sort them;
-		//// TODO(Jingwen): Check whether do we need to sort for every Cluster!!!!!!!!!!!!!!!
-		//
-		if (clusters[cm]->strand == 0) { 
-			//if (clusters[cm]->refinespace == 1) DiagonalSort<GenomeTuple>(clusters[cm]->matches.begin(), clusters[cm]->matches.end());
-			DiagonalSort<GenomeTuple>(clusters[cm]->matches.begin(), clusters[cm]->matches.end());
-		}
-		else {
-			//if (clusters[cm]->refinespace == 1) AntiDiagonalSort<GenomeTuple>(clusters[cm]->matches.begin(), clusters[cm]->matches.end());
-			AntiDiagonalSort<GenomeTuple>(clusters[cm]->matches.begin(), clusters[cm]->matches.end(), read.length);
-		}
-
-		//
-		// Linear Extension;
-		// NOTICE: anchors in "clusters[cm]" have the same strand;
-		//
-		int n = 1;
-		int m = 0;
-		while (n < clusters[cm]->matches.size()) {
-
-			long long int curDiag, nextDiag;
-			if (clusters[cm]->strand == 0) {
-				curDiag = (long long int) clusters[cm]->matches[n-1].first.pos - (long long int) clusters[cm]->matches[n-1].second.pos;
-				nextDiag = (long long int) clusters[cm]->matches[n].first.pos - (long long int) clusters[cm]->matches[n].second.pos;
-			}
-			else {
-				curDiag = (long long int) clusters[cm]->matches[n-1].first.pos + (long long int) clusters[cm]->matches[n-1].second.pos;
-				nextDiag = (long long int) clusters[cm]->matches[n].first.pos + (long long int) clusters[cm]->matches[n].second.pos;			
-			}
-
-			if (curDiag == nextDiag) { // those two anchors are on the same diagonal
-
-				if (clusters[cm]->matches[n].first.pos < clusters[cm]->matches[n-1].first.pos + opts.globalK) { // anchor n-1 and anchor n are overlapped
-					n++;
-				}
-				else { //  anchor n-1 and anchor n are not overlapped; Need to extend after anchor n-1
-					GenomePos qe, te;
-					Checkbp(clusters[cm]->matches[n-1], clusters[cm]->matches[n], genome, read, clusters[cm]->chromIndex, qe, te, clusters[cm]->strand, opts);
-					if (clusters[cm]->strand == 0 and qe == clusters[cm]->matches[n].first.pos and te == clusters[cm]->matches[n].second.pos) {
-						n++;
-					}
-					else if (clusters[cm]->strand == 1 and qe == clusters[cm]->matches[n].first.pos and te == clusters[cm]->matches[n].second.pos + opts.globalK - 1) {
-						n++;
-					}
-					else {
-						extCluster[c].matches.push_back(GenomePair(GenomeTuple(0, clusters[cm]->matches[m].first.pos), GenomeTuple(0, clusters[cm]->matches[m].second.pos)));
-						extCluster[c].matchesLengths.push_back(qe - clusters[cm]->matches[m].first.pos);
-						m = n;
-						n++;
-					}
-				}
-			}
-			else {
-				extCluster[c].matches.push_back(GenomePair(GenomeTuple(0, clusters[cm]->matches[m].first.pos), GenomeTuple(0, clusters[cm]->matches[m].second.pos)));
-				extCluster[c].matchesLengths.push_back(clusters[cm]->matches[n-1].first.pos + opts.globalK - clusters[cm]->matches[m].first.pos);
-				m = n;
+			if (pairs[n].first.pos < pairs[n-1].first.pos + opts.globalK) { // anchor n-1 and anchor n are overlapped
 				n++;
 			}
+			else { //  anchor n-1 and anchor n are not overlapped; Need to extend after anchor n-1
+				GenomePos qe, te;
+				Checkbp(pairs[n-1], pairs[n], genome, read, chromIndex, qe, te, strand, opts, mat);
+				if (qe == pairs[n].first.pos and te == pairs[n].second.pos) {
+					n++;
+				}
+				else {
+					Extendpairs.push_back(GenomePair(GenomeTuple(0, pairs[m].first.pos), GenomeTuple(0, pairs[m].second.pos)));
+					ExtendpairsMatchesLength.push_back(qe - pairs[m].first.pos);
+					m = n;
+					n++;
+				}
+			}
 		}
-
-		if (n == clusters[cm]->matches.size()) { // and m != n-1
-			extCluster[c].matches.push_back(GenomePair(GenomeTuple(0, clusters[cm]->matches[m].first.pos), GenomeTuple(0, clusters[cm]->matches[m].second.pos)));
-			extCluster[c].matchesLengths.push_back(clusters[cm]->matches[n-1].first.pos + opts.globalK - clusters[cm]->matches[m].first.pos);		
+		else {
+			Extendpairs.push_back(GenomePair(GenomeTuple(0, pairs[m].first.pos), GenomeTuple(0, pairs[m].second.pos)));
+			ExtendpairsMatchesLength.push_back(pairs[n-1].first.pos + opts.globalK - pairs[m].first.pos);
+			m = n;
+			n++;
 		}
-
-		extCluster[c].strand = clusters[cm]->strand;
-		DecideCoordinates(extCluster[c]);
 	}
-*/
+	if (n == pairs.size()) { // and m != n-1
+		Extendpairs.push_back(GenomePair(GenomeTuple(0, pairs[m].first.pos), GenomeTuple(0, pairs[m].second.pos)));
+		ExtendpairsMatchesLength.push_back(pairs[n-1].first.pos + opts.globalK - pairs[m].first.pos);		
+	}
+}
 
+
+//
+// This function takes GenomePairs ExtendPairs; vector<int> ExtendPairsMatchesLengths;
+// Note: only forward strand;
+//
+void 
+TrimOverlappedAnchors(GenomePairs &ExtendPairs, vector<int> &ExtendPairsMatchesLengths) {
+
+	//
+	// Get all long anchors
+	//
+	LongAnchors longanchors(&ExtendPairs, &ExtendPairsMatchesLengths, 0);
+	for (int ln = 0; ln < ExtendPairs.size(); ln++) {
+		if (ExtendPairsMatchesLengths[ln] >= 20) { 
+			longanchors.anchorIndex.push_back(ln);
+		}
+	}
+
+	//
+	// Catersiansort anchors and trim overlapped adjacent two;
+	//
+	longanchors.Sort();
+	for (int ln = 1; ln < longanchors.anchorIndex.size(); ln++) {
+
+		int prev = longanchors.anchorIndex[ln-1];
+		int cur = longanchors.anchorIndex[ln];
+
+		//
+		// If the adjacent anchors are overlapped less than 30b, trim the prev
+		// 
+		int overlap_r = 0, overlap_g = 0, overlap = 0;
+		bool lr = 0, lg = 0;
+		//
+		// Check if two anchors are overlapped on read;
+		//
+		if (ExtendPairs[cur].first.pos < ExtendPairs[prev].first.pos + ExtendPairsMatchesLengths[prev] 
+			and 
+			ExtendPairs[cur].first.pos >= ExtendPairs[prev].first.pos + ExtendPairsMatchesLengths[prev] - 5) {
+
+			overlap_r = ExtendPairs[prev].first.pos + ExtendPairsMatchesLengths[prev] - ExtendPairs[cur].first.pos;
+			assert(overlap_r <= 5); assert(overlap_r > 0);
+			lr = 1;
+		}				
+		//
+		// Check if two anchors are overlapped on genome;
+		//
+		if (ExtendPairs[cur].second.pos < ExtendPairs[prev].second.pos + ExtendPairsMatchesLengths[prev] 
+			and 
+			ExtendPairs[cur].second.pos >= ExtendPairs[prev].second.pos + ExtendPairsMatchesLengths[prev] - 5) {
+
+			overlap_g = ExtendPairs[prev].second.pos + ExtendPairsMatchesLengths[prev] - ExtendPairs[cur].second.pos;
+			assert(overlap_g <= 5); assert(overlap_g > 0);
+			lg = 1;
+		}
+		
+		if (overlap_r > 0 or overlap_g > 0) {
+			overlap = max(overlap_r, overlap_g);
+			ExtendPairsMatchesLengths[prev] -= overlap+1;
+			if (lr == 1) assert(ExtendPairs[cur].first.pos >= ExtendPairs[prev].first.pos + ExtendPairsMatchesLengths[prev]);
+			if (lg == 1) assert(ExtendPairs[cur].second.pos >= ExtendPairs[prev].second.pos + ExtendPairsMatchesLengths[prev]);
+		
+		}
+	}
+	
+}
 
 #endif
